259b3982e2346c2c740ab12554c262d45e937ace
quanganh-snake <tbquanganh@gmail.com>
2023-10-24 23:31:48 +0700

    update and upload api crawl: get hot-comics, new-comics, a comic, a chapter

--- backend/index.js
+++ backend/index.js
@@ -40,12 +40,12 @@ app.get("/api/v1/hot-comics", (request, response, next) => {
 			$(".listupd .hot-item > a", html).each(function () {
 				const name = $(this).attr("title") || $(this).find(".caption > h3").text() || $(this).find("> img").attr("alt");
 				const url = $(this).attr("href").split(process.env.BASE_URL)[1];
-				const thumbnail = $(this).find("> img").attr("src") || $(this).find("> img").attr("data-src");
-				console.log(`>>> line 33: Name: ${name} - URL: ${url} - thumbnail: ${thumbnail}`);
+				const thumbnail = $(this).find("> img").attr("data-src") || $(this).find("> img").attr("src");
+				// console.log(`>>> line 33: Name: ${name} - URL: ${url} - thumbnail: ${thumbnail}`);
 				posterComics.push({
 					name: name,
-					url: "http://localhost:8069/api/v1" + url,
-					thumbnail: "https://manhuarock.us" + thumbnail,
+					url: "http://localhost:8069/api/v1/" + url,
+					thumbnail: thumbnail,
 				});
 			});
 			// console.log(posterComics);
@@ -58,62 +58,84 @@ app.get("/api/v1/hot-comics", (request, response, next) => {
 
 // GET : List New Comics
 app.get("/api/v1/new-comics", (request, response) => {
+	const listNewComics = [];
 	try {
 		axios(process.env.BASE_URL).then((res) => {
 			const html = res.data;
+			const regexGetId = /\/(\d+)$/;
+
 			const $ = cheerio.load(html);
-            $(".listupd > .page-item > .bsx-item", html).each(function () {
-                const id = $(this).find(".thumb-manga > a").attr("title");;
+			$(".listupd > .page-item > .bsx-item", html).each(function () {
 				const name = $(this).find(".thumb-manga > a").attr("title") || $(this).find(".thumb-manga > a > img").attr("alt");
-				const url = $(this).find(".thumb-manga > a").attr("href");
+				const url = "http://localhost:8069/api/v1" + $(this).find(".thumb-manga > a").attr("href").split("https://manhuarockvip.com")[1];
+				const id = url.match(regexGetId)[1];
 				const thumbnail = $(this).find(".thumb-manga > a > img").attr("data-src") || $(this).find(".thumb-manga > a > img").attr("src");
-				// const comicName = $(this).find("h1.title-detail").text();
-				// const comicUpdatedAt = $(this).find("time.small").text();
-				// const comicDetailInfor = $(this).find("div.detail-info > div.row");
-				// const comicThumbnail = $(comicDetailInfor).find("> div.col-image > img").attr("src");
-				// const comicListInfor = $(comicDetailInfor).find("> div.col-info > ul.list-info");
-				// const comicOtherName = $(comicListInfor).find("> li.othername > h2").text();
-				// const comicAuthor = $(comicListInfor).find("> li.author p+p").text();
-				// const comicStatus = $(comicListInfor).find("> li.status p+p").text();
-				// // const comicKind = $(comicListInfor).find("> li.kind p+p").text();
-				// const comicKinds = [];
-				// const comicKindLinks = $(comicListInfor)
-				// 	.find("> li.kind p+p a")
-				// 	.each(function () {
-				// 		comicKinds.push({
-				// 			link: $(this).attr("href"),
-				// 			name: $(this).text(),
-				// 		});
-				// 	});
-				// // console.log("🚀 ~ file: index.js:73 ~ comicKinds:", comicKinds);
-				// const comicViews = $(comicListInfor).find("li:last-child p+p").text();
-				// const comicContent = $(this).find("div.detail-content p").text();
-				// const listChapters = [];
-				// const listChaptersSelector = $(this)
-				// 	.find("div.list-chapter > nav > ul li")
-				// 	.each(function () {
-				// 		listChapters.push({
-				// 			nameChapter: $(this).find("div.col-xs-5.chapter").text(),
-				// 			linkChapter: "http://localhost:8069/api/v1" + $(this).find("div.col-xs-5.chapter > a").attr("href").split("https://nettruyenco.vn")[1],
-				// 			updatedAtChapter: $(this).find("div.col-xs-4.no-wrap.small").text(),
-				// 		});
-				// 	});
-				// const objComic = {
-				// 	comicName,
-				// 	comicUpdatedAt,
-				// 	comicThumbnail,
-				// 	comicDetailInfo: {
-				// 		comicOtherName,
-				// 		comicAuthor,
-				// 		comicStatus,
-				// 		comicKinds,
-				// 		comicViews,
-				// 	},
-				// 	comicContent,
-				// 	listChapters,
-				// };
-				// response.status(200).json(objComic);
+				const rate = $(this).find(".bigor-manga > .item-rate .mmrate").attr("data-rating").trim() || $(this).find(".bigor-manga > .item-rate span").text().trim();
+
+				listNewComics.push({
+					id,
+					name,
+					url,
+					thumbnail,
+					rate,
+				});
 			});
+			response.status(200).json(listNewComics);
+		});
+	} catch (error) {
+		response.status(500).json(error.message);
+	}
+});
+
+// GET : A Comic
+app.get("/api/v1/truyen/:comic/:id", (request, response) => {
+	let urlComic = process.env.BASE_URL + "truyen/" + request.params.comic + "/" + request.params.id;
+	try {
+		axios(urlComic).then((res) => {
+			let dataComicDetail = {
+				id: request.params.id,
+				thumbnail: "",
+				averagerate: 0,
+				countrate: 0,
+				otherName: "",
+				authorName: "",
+				artist: "",
+				category: [],
+				comicType: "",
+				yearOfRelease: "",
+				status: "",
+				totalViews: 0,
+				description: "",
+				listChapters: [],
+			};
+			const html = res.data;
+			const $ = cheerio.load(html);
+			$(".tab-summary", html).each(function () {
+				const thumbnail = $(this).find(".summary_image > a > img").attr("src") || $(this).find(".summary_image > a > img").attr("data-src");
+				dataComicDetail = { ...dataComicDetail, thumbnail };
+			});
+
+			$(".content-manga-left", html).each(function () {
+				const description = $(this).find(".panel-story-description .dsct > p").text();
+				const listChapters = [];
+				const regexGetId = /\/(\d+)$/;
+				$(this)
+					.find(".panel-manga-chapter .row-content-chapter li")
+					.each(function () {
+						const chapterName = $(this).find("> a").text();
+						const chapterLink = "http://localhost:8069/api/v1" + $(this).find("> a").attr("href").split("https://manhuarockvip.com")[1];
+						const chapterId = chapterLink.match(regexGetId)[1];
+						const chapterCreatedAt = $(this).find("> span.chapter-time").text();
+						listChapters.push({
+							chapterId,
+							chapterName,
+							chapterLink,
+							chapterCreatedAt,
+						});
+					});
+				dataComicDetail = { ...dataComicDetail, description, listChapters };
+			});
+			response.status(200).json(dataComicDetail);
 		});
 	} catch (error) {
 		response.status(500).json(error.message);
@@ -121,9 +143,19 @@ app.get("/api/v1/new-comics", (request, response) => {
 });
 
 // GET : A Chapter
+app.get("/api/v1/truyen/:comic/:chapter/:id", (request, response) => {
+	let urlChapter = process.env.BASE_URL + `truyen/${request.params.comic}/${request.params.chapter}/${request.params.id}`;
+	try {
+        axios(urlChapter).then(res => {
+            const html = res.data;
+            
+        })
+	} catch (error) {
+		res.status(500).json(error.message);
+	}
+});
 
 // Run the server
-
 app.listen(process.env.PORT || 8069, () => {
 	console.log(`Starting server on port ${process.env.PORT}`);
 });
